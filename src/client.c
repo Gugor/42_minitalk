d172d2c73c94be6f4a411b4056e509d7ca306077
Hugo Montoya Vazquez <hmontoyavazquez@gmail.com>
2023-09-23 17:28:49 +0200

    minital basic finished

--- src/client.c
+++ src/client.c
@@ -6,27 +6,27 @@
 /*   By: hmontoya <hmontoya@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 13:08:33 by hmontoya          #+#    #+#             */
-/*   Updated: 2023/09/21 18:49:09 by hmontoya         ###   ########.fr       */
+/*   Updated: 2023/09/23 17:23:33 by hmontoya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minitalk.h"
 #include "../include/ft_printf.h"
 
-void error_handler(int argc, char *argv[])
+void	error_handler(int argc, char *argv[])
 {
-	pid_t  pid;
+	pid_t	pid;
 
 	pid = 0;
 	if (argc != 3)
 	{
 		ft_printf(":: Error! - Wrong number of arguments.\n");
-		ft_printf(":: Correct: ./client [SERVER PID] [STRING MESSAGE]\n");
+		ft_printf(":: Usage: ./client [SERVER PID] [STRING MESSAGE]\n");
 		exit(1);
 	}
 	ft_printf("Cheking pid...\n");
 	pid = atoi(argv[1]);
-	if (pid < 0)
+	if (pid <= 0)
 	{
 		ft_printf(":: Error: PID argument is not a valid number.\n");
 		exit(2);
@@ -34,14 +34,23 @@ void error_handler(int argc, char *argv[])
 	ft_printf("Access to server with PID: %d\n", pid);
 }
 
-int  encode(int pid, char bits)
+void	send_signal(pid_t pid, int signal)
 {
-	int i;
-	
+	if (kill(pid, signal) == -1)
+	{
+		perror("Error while sending signaln");
+		exit(EXIT_FAILURE);
+	}
+}
+
+int	encode(int pid, char bits)
+{
+	int	i;
+
 	i = 0;
 	if (!bits)
 		return (0);
-	while (i <  8)
+	while (i < 8)
 	{
 		if (kill(pid, 0) != 0)
 		{
@@ -50,34 +59,25 @@ int  encode(int pid, char bits)
 		}
 		if ((bits & (1 << i)) != 0)
 		{
-			if (kill(pid, SIGUSR1) == -1)
-			{
-				perror("Error while sending SIGUSR1\n");
-				exit(EXIT_FAILURE);
-			}
+			send_signal(pid, SIGUSR1);
 		}
 		else
 		{
-    		if (kill(pid, SIGUSR2) == -1)
-			{
-				perror("Error while sending SIGUSR1\n");
-				exit(EXIT_FAILURE);
-			}
+			send_signal(pid, SIGUSR2);
 		}
 		usleep(100);
 		i++;
 	}
-		return (0);
+	return (0);
 }
 
-
-int main(int argc, char *argv[]) 
+int	main(int argc, char *argv[])
 {
-	pid_t  pid;
-	int i;
-	int error;
-	char *str;
-	
+	pid_t	pid;
+	int		i;
+	int		error;
+	char	*str;
+
 	error_handler(argc, argv);
 	pid = atoi(argv[1]);
 	i = 0;
@@ -88,6 +88,6 @@ int main(int argc, char *argv[])
 		encode(pid, str[i]);
 		i++;
 	}
-	ft_printf("Message send succesfully!.\n");
-    return 0;
+	ft_printf("Message sended succesfully!.\n");
+	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -6,35 +6,43 @@
 /*   By: hmontoya <hmontoya@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 11:10:10 by hmontoya          #+#    #+#             */
-/*   Updated: 2023/09/21 18:50:01 by hmontoya         ###   ########.fr       */
+/*   Updated: 2023/09/23 17:04:58 by hmontoya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minitalk.h" 
 #include "../include/ft_printf.h" 
 
-
-void decoder(int sig)
+void	decoder(int sig)
 {
-	static int i = 0;
-	static int c = 0;
+	static int	i = 0;
+	static int	c = 0;
 
+	if (!(sig == SIGUSR1 || sig == SIGUSR2))
+	{
+		perror("Unexpected signal recived\n");
+		exit(1);
+	}
 	if (sig == SIGUSR1)
-		c = c | 1 << i ;		
+		c = c | 1 << i;
 	i++;
 	if (i == 8)
 	{
-		write(1,&c,1);
+		if (write(1, &c, 1) == -1)
+		{
+			perror("write");
+			exit(1);
+		}
 		i = 0;
 		c = 0;
 	}
 }
 
-int main(void)
+int	main(void)
 {
-	signal(SIGUSR1, &decoder); 
+	signal(SIGUSR1, &decoder);
 	signal(SIGUSR2, &decoder);
-	ft_printf("SERVER PID: %d\n",getpid());
+	ft_printf("SERVER PID: %d\n", getpid());
 	sleep(1);
 	while (1)
 		sleep(1);
